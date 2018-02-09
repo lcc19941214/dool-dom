3d8f31d8bbfcb2f283483a0649631508d1824886
lcc19941214 <245987239@qq.com>
2018-02-09 18:11:48 +0800

    chore: update example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import Element from './element';
 import RenderDOM from './renderDOM';
-import Component from './component';
 import diff from './diff';
 import update from './update';
 
-export { Component, Element, RenderDOM, diff, update };
+export { Element, RenderDOM, diff, update };
